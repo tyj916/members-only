ca6105915f19540c9549cc66a52ec0f043ddf7b9
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-28 01:06:57 +0800

    Add sign up form route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController');
 const userRouter = Router();
 
 userRouter.get('/sign-up', userController.createUserGet);
+userRouter.post('/sign-up', userController.createUserPost);
 userRouter.get('/log-in', userController.logInGet);
 userRouter.get('/log-out', userController.logOut);
 userRouter.get('/membership', userController.joinMembershipGet);
