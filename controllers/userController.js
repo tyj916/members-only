2ae02550f179c7c625d6570f1da0423e5e96c50d
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-28 01:07:22 +0800

    Create sign up form validators

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,46 @@
+const { body, validationResult} = require('express-validator');
+
+const alphaErr = 'must only contain letters.';
+const nameLengthErr = 'must be between 1 and 20 characters.';
+
+const validateUser = [
+  body('firstName').trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 20 }).withMessage(`First name ${nameLengthErr}`),
+  body('lastName').trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 20 }).withMessage(`Last name ${nameLengthErr}`),
+  body('username').trim()
+    .isLength({ min: 1, max: 255}).withMessage('Username must not exceed 255 characters.')
+    .custom(async (value) => {
+
+    }).withMessage('Username already in use'),
+  body('password')
+    .isLength({ min: 6 }).withMessage(`Password must be at least 6 characters`),
+  body('passwordConfirmation')
+    .custom((value, {req}) => {
+      return value === req.body.password
+    }).withMessage('Password do not match'),
+];
+
 async function createUserGet(req, res) {
   res.render('user/signUp');
 }
+
+const createUserPost = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('user/signUp', {
+        errors: errors.array(),
+      });
+    }
+
+    const { firstName, lastName, username, password } = req.body;
+    res.redirect('sign-up');
+  }
+]
 
 async function logInGet(req, res) {
   res.render('user/logIn');
@@ -22,6 +62,7 @@ async function joinMembershipGet(req, res) {
 
 module.exports = {
   createUserGet,
+  createUserPost,
   logInGet,
   logOut,
   getUserDetails,
