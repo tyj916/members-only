e1bec4598bcee81161c126605aa9951869323083
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-28 01:06:08 +0800

    Add express extended urlencoded

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const postsRouter = require('./routes/postsRouter');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('index');
