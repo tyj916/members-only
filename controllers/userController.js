205f524e7fe78ecb7cb5275bfce5d28d6c08d70f
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-28 23:48:00 +0800

    Add same username validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,13 +19,18 @@ const validateSignUp = [
   body('username').trim()
     .isLength({ min: 1, max: 255}).withMessage('Username must not exceed 255 characters.')
     .custom(async (value) => {
-
-    }).withMessage('Username already in use'),
+      const user = await db.getUserByUsername(value);
+      if (user) {
+        // Asynchronous validator will return a promise
+        // Reject the promise to make it invalid
+        throw new Error('Username already in use');
+      }
+    }),
   body('password')
     .isLength({ min: 6 }).withMessage(`Password must be at least 6 characters`),
   body('passwordConfirmation')
     .custom((value, {req}) => {
-      return value === req.body.password
+      return value === req.body.password;
     }).withMessage('Password do not match'),
 ];
 
