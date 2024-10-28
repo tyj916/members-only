2a53f6de99023d44189243fe765a99782eed279d
Tan Yi Jun <tanyijun6@gmail.com>
2024-10-28 23:25:42 +0800

    Remodule validation middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,10 @@ const db = require('../db/queries');
 const alphaErr = 'must only contain letters.';
 const nameLengthErr = 'must be between 1 and 20 characters.';
 
+async function createUserGet(req, res) {
+  res.render('user/signUp');
+}
+
 const validateSignUp = [
   body('firstName').trim()
     .isAlpha().withMessage(`First name ${alphaErr}`)
@@ -25,6 +29,30 @@ const validateSignUp = [
     }).withMessage('Password do not match'),
 ];
 
+async function createUserPost(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('user/signUp', {
+      errors: errors.array(),
+    });
+  }
+
+  const { firstName, lastName, username, password } = req.body;
+  bcrypt.hash(password, 10, async (err, hashedPassword) => {
+    if (err) {
+      throw new Error('Password encrypt error');
+    }
+
+    await db.createUser(firstName, lastName, username, hashedPassword);
+  });
+  res.redirect('sign-up');
+}
+
+
+async function logInGet(req, res) {
+  res.render('user/logIn');
+}
+
 const validateLogIn = [
   body('username').trim()
     .isLength({ min: 1, max: 255}).withMessage('Username must not exceed 255 characters.'),
@@ -32,59 +60,26 @@ const validateLogIn = [
     .isLength({ min: 6 }).withMessage(`Password must be at least 6 characters`),
 ];
 
-async function createUserGet(req, res) {
-  res.render('user/signUp');
-}
-
-const createUserPost = [
-  validateSignUp,
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).render('user/signUp', {
-        errors: errors.array(),
-      });
-    }
-
-    const { firstName, lastName, username, password } = req.body;
-    bcrypt.hash(password, 10, async (err, hashedPassword) => {
-      if (err) {
-        throw new Error('Password encrypt error');
-      }
-
-      await db.createUser(firstName, lastName, username, hashedPassword);
+async function logInPost(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('user/logIn', {
+      errors: errors.array(),
     });
-    res.redirect('sign-up');
   }
-]
 
-async function logInGet(req, res) {
-  res.render('user/logIn');
+  const { username, password } = req.body;
+  const user = await db.getUserByUsername(username);
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    console.log('Login failed. Password incorrect');
+  } else {
+    console.log('Login successful.');
+  }
+
+  res.redirect('log-in');
 }
-
-const logInPost = [
-  validateLogIn,
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).render('user/logIn', {
-        errors: errors.array(),
-      });
-    }
-
-    const { username, password } = req.body;
-    const user = await db.getUserByUsername(username);
-    const match = await bcrypt.compare(password, user.password);
-
-    if (!match) {
-      console.log('Login failed. Password incorrect');
-    } else {
-      console.log('Login successful.');
-    }
-
-    res.redirect('log-in');
-  },
-];
 
 async function logOut(req, res) {
   res.send('logout');
@@ -102,8 +97,10 @@ async function joinMembershipGet(req, res) {
 
 module.exports = {
   createUserGet,
+  validateSignUp,
   createUserPost,
   logInGet,
+  validateLogIn,
   logInPost,
   logOut,
   getUserDetails,
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,9 +3,9 @@ const userController = require('../controllers/userController');
 const userRouter = Router();
 
 userRouter.get('/sign-up', userController.createUserGet);
-userRouter.post('/sign-up', userController.createUserPost);
+userRouter.post('/sign-up', userController.validateSignUp, userController.createUserPost);
 userRouter.get('/log-in', userController.logInGet);
-userRouter.post('/log-in', userController.logInPost);
+userRouter.post('/log-in', userController.validateLogIn, userController.logInPost);
 userRouter.get('/log-out', userController.logOut);
 userRouter.get('/membership', userController.joinMembershipGet);
 userRouter.get('/u/:username', userController.getUserDetails);
