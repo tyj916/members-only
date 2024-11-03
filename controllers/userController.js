a640525eb812a4fe0e839d8f1f76f5bfd078355a
Tan Yi Jun <tanyijun6@gmail.com>
2024-11-03 16:45:40 +0800

    Fix user variable collision at user details page

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,7 +65,11 @@ async function renderUserDetails(req, res) {
 
   const posts = await db.getPostsByUserId(user.id);
   res.render('user/userDetails', {
-    user: user,
+    username: user.username,
+    first_name: user.first_name,
+    last_name: user.last_name,
+    membership_status: user.membership_status,
+    user: req.user,
     posts: posts,
   });
 }
